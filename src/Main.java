88f4de77c130a6d1d25fae5fa9c5c3baac1d2d1f
Tai-NgonHeng <heng68807@gmail.com>
2024-08-26 13:44:00 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is my Booom!");
+        System.out.println("this is my the test on github!");
     }
 }
