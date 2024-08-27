a1bb0b4f5af6faee38dd3fb42acc6230b5c08cc4
watermelon <heng68807@gmail.com>
2024-08-27 18:06:51 +0700

    test

--- src/Oop/Parent.java
+++ src/Oop/Parent.java
@@ -4,15 +4,35 @@ public class Parent {
   public static void main(String[] args) {
     car car1 = new car("tesla","2024 ","red",2);
     car car2 = new car("BMW", "M5 CS","Black",4);
+    car car3 = new car();
+//    car Car = new car();
     System.out.println("This is the Car II :"+car2.getName());
     System.out.println(car2.getModel());
     System.out.println(car2.getColor());
     System.out.println(car2.getDoor());
+    car2.move();
 
     System.out.println("This is the Car I :"+car1.getName());
     System.out.println(car1.getModel());
     System.out.println(car1.getColor());
     System.out.println(car1.getDoor());
+    car1.stop();
+
 
   }
 }
+class plane{
+  private String name;
+  private String model;
+  private String color;
+  private int door;
+  private int engine;
+  public plane(String name, String model, String color, int door, int engine){
+    this.name = name;
+    this.model= model;
+    this.color= color;
+    this.door = door;
+    this.engine = engine;
+  }
+}
+
--- src/Oop/car.java
+++ src/Oop/car.java
@@ -1,50 +1,55 @@
 package Oop;
 
 public class car {
-  private String name;
-  private String model;
-  private String color;
-  private int door;
+    private String name;
+    private String model;
+    private String color;
+    private int door;
 
-  public car(String name, String model, String color, int door){
-    this.name = name;
-    this.model = model;
-    this.color = color;
-    this.door = door ;
-  }
-  public void move(){
-    System.out.println("the car "+ this.name + " is moving");
-  }
-  public void stop(){
-    System.out.println("the car "+this.name+" is parking");
-  }
-  //getname return name get name this.name = name
-  public String getName(){
-    return name;
-  }
-  public void setName(){
-    this.name = name;
-  }
+    public car(String name, String model, String color, int door){
+        this.name = name;
+        this.model = model;
+        this.color = color;
+        this.door = door ;
+    }
+    public car(){
+        this("BMW", "M5 CS","white",4);
+    }
+    public void move(){
+        System.out.println("the car "+ this.name + " is moving");
+    }
+    public void stop(){
+        System.out.println("the car "+this.name+" is parking");
+    }
 
-  public String getModel(){
-    return model;
-  }
-  public void setModel(){
-    this.model = model;
-  }
 
-  public String getColor(){
-    return color;
-  }
+    //getname return name set name this.name = name
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name = name;
+    }
 
-  public void setColor(){
-    this.color = color;
-  }
+    public String getModel(){
+        return model;
+    }
+    public void setModel(){
+        this.model = model;
+    }
 
-  public int getDoor(){
-    return door;
-  }
-  public void setDoor(){
-    this.door = door;
-  }
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(){
+        this.color = color;
+    }
+
+    public int getDoor(){
+        return door;
+    }
+    public void setDoor(){
+        this.door = door;
+    }
 }
